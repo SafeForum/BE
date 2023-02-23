80ed504cff7d53ceca018e6e4e15bc60ca5b3dbe
Gabriel Henton <59454894+gsauce7@users.noreply.github.com>
2023-02-23 10:51:52 -0800

    Thread creator must match userId

--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -167,7 +167,7 @@ module.exports = buildSchema(`
         deleteComment(commentId: String!): Comment!
         deleteThread(threadId: String!, userId: String!): Thread!
         deleteComment(commentId: String!): Comment!
-        deleteThread(threadId: String!): Thread!
+        deleteThread(threadId: String!, userId: String!): Thread!
     }
 
     schema {
