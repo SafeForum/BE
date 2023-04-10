2f981ac795631c41ec3014fdbae083e2afb5d263
unknown <nandhinimadan20@gmail.com>
2023-04-10 11:22:59 -0700

    Issue in updating password. Password getting updated to the first data

--- graphql/resolvers/password.js
+++ graphql/resolvers/password.js
@@ -2,38 +2,47 @@ const bcrypt = require("bcryptjs");
 
 //models
 const User = require("../../models/user");
-const password=require("../../models/password")
+const password = require("../../models/password")
 
 module.exports = {
     editPassword: async (args, req) => {
         try {
-            console.log("editPassword");
-            const hashedOldPassword= await bcrypt.hash(args.oldPassword);
-            console.log("harshing");
-            const hashedNewPassword=await bcrypt.hash(args.Password);
-            
+           
+            const hashedOldPassword = await bcrypt.hash(args.oldPassword,12);
+            console.log("harshing",hashedOldPassword);
+            const hashedNewPassword = await bcrypt.hash(args.password,12);
+
             const user = await User.findById(args.userId);
-            
+            console.log("user Id",args.userId);
+            console.log("user details",user)
+
             if (!user) {
                 throw new Error("User does not exit")
             }
-            const isEqual = await bcrypt.compare(req.password, args.oldPassword);
+            const update={
+                password:hashedNewPassword
+            }
+            const isEqual = await bcrypt.compare(args.oldPassword, user.password);
+            console.log("isEqual",isEqual)
             if (!isEqual) {
                 throw new Error("Password is incorrect!");
             }
-            try{
-                const updatePassword= await User.findOneAndUpdate(args.userId,hashedNewPassword);
-                if(!updatePassword){
+            try {
+                const updatePassword = await User.findOneAndUpdate(args.userId, update,{new:true});
+                console.log("updatedpassword",updatePassword)
+                if (!updatePassword) {
                     throw new Error("Password cannot be updated")
                 }
+                return user
             }
-            catch(err){
+            catch (err) {
                 throw err;
             }
-            
+
 
         }
         catch (err) {
+            console.log("error")
             throw err
         }
     }
