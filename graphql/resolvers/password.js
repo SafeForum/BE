571ae8933e436829b40b1462b3d4b8ea8dd1b6d1
unknown <nandhinimadan20@gmail.com>
2023-04-10 12:14:31 -0700

    Cleaned up and good to merge

--- graphql/resolvers/password.js
+++ graphql/resolvers/password.js
@@ -1,5 +1,4 @@
 const bcrypt = require("bcryptjs");
-
 //models
 const User = require("../../models/user");
 const password = require("../../models/password")
@@ -8,14 +7,9 @@ module.exports = {
     editPassword: async (args, req) => {
         try {
            
-            const hashedOldPassword = await bcrypt.hash(args.oldPassword,12);
-            console.log("harshing",hashedOldPassword);
+            const hashedOldPassword = await bcrypt.hash(args.oldPassword,12);     
             const hashedNewPassword = await bcrypt.hash(args.password,12);
-
             const user = await User.findById(args.userId);
-            console.log("user Id",args.userId);
-          //  console.log("user details",user)
-
             if (!user) {
                 throw new Error("User does not exit")
             }
@@ -23,14 +17,11 @@ module.exports = {
                 password:hashedNewPassword
             }
             const isEqual = await bcrypt.compare(args.oldPassword, user.password);
-            console.log("isEqual",isEqual)
             if (!isEqual) {
                 throw new Error("Password is incorrect!");
             }
             try {
                 const updatePassword = await User.findOneAndUpdate({_id:user}, update,{new:true});
-                console.log("updatedpassword",updatePassword)
-
                 if (!updatePassword) {
                     throw new Error("Password cannot be updated")
                 }
@@ -39,11 +30,8 @@ module.exports = {
             catch (err) {
                 throw err;
             }
-
-
         }
         catch (err) {
-            console.log("error")
             throw err
         }
     }
