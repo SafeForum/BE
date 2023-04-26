1e48260cb29d22c7236785396b16a3e0e1b31a4d
koyamot <koyamot@gmail.com>
2023-04-26 11:53:40 -0700

    clean up password

--- graphql/resolvers/password.js
+++ graphql/resolvers/password.js
@@ -1,36 +1,37 @@
 const bcrypt = require("bcryptjs");
 //models
 const User = require("../../models/user");
-const password = require("../../models/password")
 
 module.exports = {
-    editPassword: async (args, req) => {
-        try {   
-            const hashedNewPassword = await bcrypt.hash(args.password,12);
-            const user = await User.findById(args.userId);
-            if (!user) {
-                throw new Error("User does not exit")
-            }
-            const update={
-                password:hashedNewPassword
-            }
-            const isEqual = await bcrypt.compare(args.oldPassword, user.password);
-            if (!isEqual) {
-                throw new Error("Password is incorrect!");
-            }
-            try {
-                const updatePassword = await User.findOneAndUpdate({_id:user}, update,{new:true});
-                if (!updatePassword) {
-                    throw new Error("Password cannot be updated")
-                }
-                return user
-            }
-            catch (err) {
-                throw err;
-            }
+  editPassword: async (args, req) => {
+    try {
+      const hashedNewPassword = await bcrypt.hash(args.password, 12);
+      const user = await User.findById(args.userId);
+      if (!user) {
+        throw new Error("User does not exit");
+      }
+      const update = {
+        password: hashedNewPassword,
+      };
+      const isEqual = await bcrypt.compare(args.oldPassword, user.password);
+      if (!isEqual) {
+        throw new Error("Password is incorrect!");
+      }
+      try {
+        const updatePassword = await User.findOneAndUpdate(
+          { _id: user },
+          update,
+          { new: true }
+        );
+        if (!updatePassword) {
+          throw new Error("Password cannot be updated");
         }
-        catch (err) {
-            throw err
-        }
+        return user;
+      } catch (err) {
+        throw err;
+      }
+    } catch (err) {
+      throw err;
     }
-}
+  },
+};
